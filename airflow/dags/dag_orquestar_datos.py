7ec622677fba7471bfa1e1924b00519561ca944c
mariana.jaramillo <marianajaramillo.20057@gmail.com>
2024-11-29 21:59:15 -0500

    docs: eliminar comentarios innecesarios.

--- airflow/dags/dag_orquestar_datos.py
+++ airflow/dags/dag_orquestar_datos.py
@@ -5,7 +5,6 @@ from datetime import datetime, timedelta
 from scripts.msg_kafka_postgres import obtener_registros_nuevos, enviar_a_kafka, actualizar_conteos
 
 
-# Configuración predeterminada del DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -13,7 +12,6 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-# Definición del DAG
 dag = DAG(
     dag_id='orquestar_datos',
     default_args=default_args,
@@ -23,16 +21,15 @@ dag = DAG(
     catchup=False,
 )
 
-# Función que orquesta el flujo completo
 def flujo_postgres():
     """Flujo de orquestación para PostgreSQL."""
     registros_nuevos = obtener_registros_nuevos()
     enviar_a_kafka(registros_nuevos, topico="postgres")
     actualizar_conteos()
 
-# Sensor para tabla "medicamentos" en PostgreSQL
+
 verificar_datos_nuevos = SqlSensor(
-    task_id='verificar_datos_nuevos',  # Esta es la primera tarea
+    task_id='verificar_datos_nuevos',
     conn_id='conexion_postgres',
     sql="""
         SELECT COUNT(*) > (
@@ -48,23 +45,20 @@ verificar_datos_nuevos = SqlSensor(
     dag=dag,
 )
 
-# Tarea para obtener los nuevos registros de la tabla "medicamentos"
 obtener_nuevos_registros = PythonOperator(
-    task_id='obtener_registros_nuevos',  # Segunda tarea
+    task_id='obtener_registros_nuevos',
     python_callable=obtener_registros_nuevos,
     provide_context=True,
     dag=dag,
 )
 
-# Tarea para enviar los datos a Kafka
 enviar_datos_a_kafka = PythonOperator(
-    task_id='enviar_datos_a_kafka',  # Tercera tarea
+    task_id='enviar_datos_a_kafka',
     python_callable=enviar_a_kafka,
     provide_context=True,
     dag=dag,
 )
 
-# Tarea para actualizar la tabla "conteos" después de enviar los datos
 actualizar_tabla_conteos = PythonOperator(
     task_id='actualizar_tabla_conteos',  # Cuarta tarea
     python_callable=actualizar_conteos,
@@ -72,5 +66,4 @@ actualizar_tabla_conteos = PythonOperator(
     dag=dag,
 )
 
-# Definir el flujo de tareas: asegurar que se ejecuten en orden
 verificar_datos_nuevos >> obtener_nuevos_registros >> enviar_datos_a_kafka >> actualizar_tabla_conteos
