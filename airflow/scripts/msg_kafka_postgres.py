2f6936c5abe432a55fc5649b44d70fca10278621
mariana.jaramillo <marianajaramillo.20057@gmail.com>
2024-11-30 11:03:28 -0500

    feat: actualiza creacion de topico.

--- airflow/scripts/msg_kafka_postgres.py
+++ airflow/scripts/msg_kafka_postgres.py
@@ -1,5 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
-from kafka import KafkaProducer
+import json
+from kafka import KafkaProducer, KafkaAdminClient
+from kafka.admin import NewTopic
 import json
 
 def obtener_registros_nuevos(**kwargs):
@@ -21,16 +23,42 @@ def obtener_registros_nuevos(**kwargs):
     kwargs['ti'].xcom_push(key='registros_nuevos', value=registros_nuevos)
     return registros_nuevos
 
+
 def enviar_a_kafka(**kwargs):
+    bootstrap_servers = 'kafka1:9092'
+    topico = 'postgres'
+    
+    # Crear un cliente de administración para verificar y crear el tópico si no existe
+    admin_client = KafkaAdminClient(bootstrap_servers=bootstrap_servers)
+    
+    # Verificar si el tópico existe
+    topics = admin_client.list_topics()
+    if topico not in topics:
+        # Crear el tópico si no existe
+        topic = NewTopic(name=topico, num_partitions=1, replication_factor=1)
+        admin_client.create_topics(new_topics=[topic], validate_only=False)
+        print(f"Tópico '{topico}' creado.")
+    else:
+        print(f"Tópico '{topico}' ya existe.")
+    
+    # Crear el productor de Kafka
     productor_kafka = KafkaProducer(
-        bootstrap_servers='kafka1:9092',
+        bootstrap_servers=bootstrap_servers,
         value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
+    
+    # Obtener los registros nuevos desde XCom
     registros_nuevos = kwargs['ti'].xcom_pull(key='registros_nuevos', task_ids='obtener_registros_nuevos')
-    topico = 'postgres'
+    
+    # Enviar los registros al tópico 'postgres'
     for registro in registros_nuevos:
         productor_kafka.send(topico, registro)
+    
+    # Asegurarse de que todos los mensajes se envíen
     productor_kafka.flush()
+
+    print(f"Se enviaron {len(registros_nuevos)} registros al tópico '{topico}'.")
+
 
 def actualizar_conteos(**kwargs):
     conexion_postgres = PostgresHook(postgres_conn_id="conexion_postgres")
